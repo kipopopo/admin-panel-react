10d3bfe1832970ff23998ad408854d7cd38309d3
kipopopo <thaqifabdaziz@gmail.com>
2020-12-06 21:17:08 +0800

    Multiple changes and CSS/icons additions

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import {ReactComponent as IconMenu} from '../assets/icons/menu-hamburger.svg';
+import {ReactComponent as IconBell} from '../assets/icons/bell.svg'
+import {ReactComponent as IconProfile} from '../assets/icons/user-profile.svg'
 
 export class Header extends React.Component {
     render() {
         return (
-            <nav className="header">
-                <p>Header component</p>
+            <nav id="header navbar-expand" className="header">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#header">
+                            <IconMenu className="nav-link-icon" />
+                        </a>
+                    </li>
+                </ul>
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#header">
+                            <IconBell className="nav-link-icon" />
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#header">
+                            <IconProfile className="nav-link-icon" />
+                        </a>
+                    </li>
+                </ul>
             </nav>
         )
     }
