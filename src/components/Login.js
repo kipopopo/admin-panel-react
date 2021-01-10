0cb4953fc73fb4b473405386cde2f66bde846527
kipopopo <thaqifabdaziz@gmail.com>
2021-01-11 02:07:45 +0800

    a little bit of styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -148,7 +148,7 @@ const Login = (props) => {
           <div className="flex justify-center">
             <button
               className="
-                group relative w-1/3 flex justify-center py-3 px-4 
+                group relative w-1/3 flex justify-center py-3 px-20 
                 bg-black bg-opacity-50 rounded-md border border-transparent 
                 text-yellow-200 text-xl font-bold 
                 transition-all duration-200
@@ -158,24 +158,49 @@ const Login = (props) => {
               disabled={loading}
             >
               <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                <svg
-                  className="
+                {loading ? (
+                  <svg
+                    class="animate-spin h-5 w-5 text-yellow-500 
+                  transition-all duration-200
+                  group-hover:text-yellow-200"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      class="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      stroke-width="4"
+                    ></circle>
+                    <path
+                      class="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                ) : (
+                  <svg
+                    className="
                     h-5 w-5 text-yellow-500 
                     transition-all duration-200
                     group-hover:text-yellow-200"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                )}
               </span>
-              {loading ? "Loading..." : "Login"}
+              {loading ? "Loading" : "Login"}
             </button>
           </div>
         </form>
