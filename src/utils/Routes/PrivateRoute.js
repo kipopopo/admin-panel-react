3a1203a7d6116c729de9a58b3c41d47fbf76eadd
kipopopo <thaqifabdaziz@gmail.com>
2021-01-12 03:23:01 +0800

    changing directory for prub/priv route file

--- src/utils/Routes/PrivateRoute.js
+++ src/utils/Routes/PrivateRoute.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import { getToken } from "./Common";
+import { getToken } from "../Common";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   return (
