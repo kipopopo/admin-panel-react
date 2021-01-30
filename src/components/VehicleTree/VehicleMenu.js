6ce0fb2d2e956e1a8bbf22c5cb903b780873f6b3
Thaqif Aziz <thaqifabdaziz@gmail.com>
2021-01-30 23:04:09 +0800

    minor changes

--- src/components/VehicleTree/VehicleMenu.js
+++ src/components/VehicleTree/VehicleMenu.js
@@ -1,6 +1,9 @@
 import React, { memo, useState } from "react";
 import { useSpring, a } from "react-spring";
 
+import { Icon } from "@iconify/react";
+import carIcon from "@iconify-icons/fa-solid/car";
+
 import { useMeasure, usePrevious } from "./helpers";
 import { Frame, Title, Content, toggle } from "../../styles/styles";
 import * as Icons from "../../assets/icons/IconTree";
@@ -17,8 +20,10 @@ const Tree = memo(({ children, name, style, defaultOpen = false }) => {
       transform: `translate3d(${isOpen ? 0 : 20}px,0,0)`,
     },
   });
-  const Icon =
+
+  const IconT =
     Icons[`${children ? (isOpen ? "Minus" : "Plus") : "Close"}SquareO`];
+
   return (
     <Frame>
       <div
@@ -28,10 +33,11 @@ const Tree = memo(({ children, name, style, defaultOpen = false }) => {
           alignItems: "center",
         }}
       >
-        <Icon
+        <IconT
           style={{ ...toggle, opacity: children ? 1 : 0.3 }}
           onClick={() => setOpen(!isOpen)}
         />
+        {name === "Cars" ? <Icon icon={carIcon} className="mr-2" /> : null}
         <Title style={style}>{name}</Title>
       </div>
       <Content
@@ -67,14 +73,18 @@ export default function VehicleMenu() {
             px-4"
         >
           <Tree name="Vehicles" defaultOpen style={styles.title}>
-            <Tree name="Cars" />
+            <Tree name="Cars">
+              <Tree name="Car 1" />
+              <Tree name="Car 2" />
+              <Tree name="Car 3" />
+              <Tree name="Car 4" />
+              <Tree name="Car 5" />
+            </Tree>
             <Tree name="Trucks">
-              <Tree name="hello" />
-              <Tree name="sub-subtree with children">
-                <Tree name="child 1" style={{ color: "#37ceff" }} />
-                <Tree name="child 2" style={{ color: "#37ceff" }} />
-                <Tree name="child 3" style={{ color: "#37ceff" }} />
-                <Tree name="custom content">
+              <Tree name="Truck 1" />
+              <Tree name="Truck 2">
+                <Tree name="Truck name here (kot)" />
+                <Tree name="Misc contents here">
                   <div
                     style={{
                       position: "relative",
@@ -87,16 +97,15 @@ export default function VehicleMenu() {
                       style={{
                         width: "100%",
                         height: "100%",
-                        background: "black",
+                        background: "white",
                         borderRadius: 5,
                       }}
                     />
                   </div>
                 </Tree>
               </Tree>
-              <Tree name="hello" />
             </Tree>
-            <Tree name="Vans" />
+            <Tree name="Motorcars" />
           </Tree>
         </div>
       </aside>
